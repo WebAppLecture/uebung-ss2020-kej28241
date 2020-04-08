d3e37c82c00aeb3f6c71649dc8c9298513ca213d
Johannes Keil <johannes.keil@physik.uni-regensburg.de>
2020-04-08 16:55:39 +0200

    add_Solution_Übung3

--- 03-CSS/src/js/index.js
+++ 03-CSS/src/js/index.js
@@ -1,12 +1,12 @@
 import { SkinChanger } from "./SkinChanger.js";
 
 let skinStyle = document.querySelector("#skin"),
-    skins = ["gold","peach","basic","win95","mech"];
+    skins = ["newskin","gold","peach","basic","win95","mech","keili"];
 
 window.skinChanger = new SkinChanger(skinStyle, skins, "./src/css/");
 
 document.querySelector(".next").addEventListener("click", () => skinChanger.next());
 document.querySelector(".previous").addEventListener("click", () => skinChanger.previous());
 
-skinChanger.activeSkin = "basic";
+skinChanger.activeSkin = "newskin";
 
